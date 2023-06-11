ffed6dccf603c1198279f186058dd3a5a304bd0f
Razieh Abedi <raziyeabedi@gmail.com>
2023-06-11 22:14:57 +0800

    added functionality to the cart

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,10 @@
 import classes from './CartButton.module.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { uiActions } from '../../store/ui-slice';
 
 const CartButton = (props) => {
-const dispatch = useDispatch()
-
+const dispatch = useDispatch();
+const cartQuantity =useSelector(state=> state.cart.totalQuantity)
 const toggleHandler = ()=>{
     dispatch(uiActions.toggle())
 }
@@ -12,7 +12,7 @@ const toggleHandler = ()=>{
   return (
     <button className={classes.button} onClick={toggleHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
